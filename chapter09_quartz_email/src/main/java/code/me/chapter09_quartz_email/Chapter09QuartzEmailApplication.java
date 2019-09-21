69d9b02c696df8fcf96920245d5271026cb1082b
nayunfeng <921769825@qq.com>
2019-09-21 09:01:07 +0800

    一步一步学习SpringBoot工程初始化

---
+++ chapter09_quartz_email/src/main/java/code/me/chapter09_quartz_email/Chapter09QuartzEmailApplication.java
@@ -0,0 +1,13 @@
+package code.me.chapter09_quartz_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter09QuartzEmailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter09QuartzEmailApplication.class, args);
+    }
+
+}
---
+++ chapter09_quartz_email/src/test/java/code/me/chapter09_quartz_email/Chapter09QuartzEmailApplicationTests.java
@@ -0,0 +1,16 @@
+package code.me.chapter09_quartz_email;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chapter09QuartzEmailApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
